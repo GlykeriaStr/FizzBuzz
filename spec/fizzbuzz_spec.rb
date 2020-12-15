c43175e4e1798e55ca39ad0ebd5594507b37eff2
Glykeria Stravodimou <strglykeria@gmail.com>
2020-12-15 16:28:54 +0000

    Fizzbuzz test complete

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,23 +1,20 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizzbuzz" when number % 5 == 0 && number % 3 == 0' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizz" when number % 3 == 0' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when number % 5 == 0' do
     expect(fizzbuzz(5)).to eq 'buzz'
-  end
-
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
   it 'returns 7 when passed 7' do
     expect(fizzbuzz(7)).to eq 7
   end
 
-  it 'returns "Try again" when passed (!Integer)' do
-    expect(fizzbuzz(!Integer)).to eq "Try again"
-  end
 end
